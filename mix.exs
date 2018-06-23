4f61a35768b2f8ec0bca5e8e8110cdce4cf900c5
@anulman <anulman@gmail.com>
2018-06-23 14:10:32 -0400

    chore(version): bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Microdata.MixProject do
   def project do
     [
       app: :microdata,
-      version: "0.0.1",
+      version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       description: description(),
