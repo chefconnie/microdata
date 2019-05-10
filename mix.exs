4391a40f7cdbe78f80147c76290e82770cabf092
@anulman <anulman@gmail.com>
2019-05-10 17:38:37 -0400

    uptick version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Microdata.MixProject do
   def project do
     [
       app: :microdata,
-      version: "0.1.9",
+      version: "0.2.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
